9861e301161a5f2ef74f1232fe943fb7cf7cefff
geldun <85728484+geldun@users.noreply.github.com>
2021-08-03 01:10:56 +0700

    Update main.js

--- menu/main.js
+++ menu/main.js
@@ -1,4 +1,4 @@
 for (i = 1; i < 9; i++) {
 
-   document.getElementById('menu_quiz').innerHTML += `<a href="https://geldun.github.io/1000_words/1000_quiz_${i}/" class="quiz">Quiz ${i}</a>`;
+   document.getElementById('menu_quiz').innerHTML += `<a href="https://geldun.github.io/1000_words/1000_quiz_${i}/index.html" class="quiz">Quiz ${i}</a>`;
 }
